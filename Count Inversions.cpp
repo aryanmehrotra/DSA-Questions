c354ee5ddebd0008cd859f699c09e598b04dffd4
mehrotra234 <44036979+mehrotra234@users.noreply.github.com>
2021-09-04 23:49:56 +0530

    Update Count Inversions.cpp

--- Count Inversions.cpp
+++ Count Inversions.cpp
@@ -1,40 +1,97 @@
-long long int mergesort(long long int *arr,long long int l,long long int mid,long long int r,long long int*temp){
-        long long int i=l,j=mid+1,k=l,total=0;  // Here
-        while(i<=mid and j<=r){
-            if(arr[i]<=arr[j]){
-                temp[k++]=arr[i++];
-            }else{
-                total+=mid-i+1;
-                temp[k++]=arr[j++];
-            }
-        }
-        while(i<=mid){
-            temp[k++]=arr[i++];
-        }
-        while(j<=r){
-            temp[k++]=arr[j++];
-        }
-        for(int i=l;i<=r;i++){
-            arr[i]=temp[i];
-        }
-        return total;
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    // Function to count inversions in the array.
+    void merge(long long arr[],int low,int high,int mid,long long &count){
+    int t1 = mid-low+1;
+    int t2 = high-mid;
+    int first[t1],second[t2];
+    for(int i=0;i<t1;i++){
+        first[i] = arr[low+i];
     }
-    long long int merge(long long int *arr,long long int l,long long int r,long long int *temp){
-        long long int mid=(l+r)/2;
-        long long int inv=0;
-        if(l<r){
-            inv+=merge(arr,l,mid,temp);
-            inv+=merge(arr,mid+1,r,temp);
-            inv+=mergesort(arr,l,mid,r,temp);
-        }
-        return inv;
-        
+    for(int i=0;i<t2;i++){
+        second[i] = arr[mid+1+i];
     }
-    long long int inversionCount(long long int arr[], long long int N)
+    
+    int a=0,b=0;
+    while(a<t1 and b<t2){
+        if(first[a] > second[b]){
+            count= (count + (t1-a));
+            b++;
+            
+        }
+        else a++;
+    }
+    
+    a = 0,b=0;
+    int k = low;
+    while(a<t1 and b<t2){
+        if(first[a] < second[b]){
+            arr[k] = first[a];
+            a++;
+        }
+        else{
+            arr[k] = second[b];
+            b++;
+        }
+        k++;
+    }
+    while(a<t1){
+        arr[k++] = first[a++];
+    }
+    while(b<t2){
+        arr[k++] = second[b++];
+    }
+    
+}
+
+void mergeSort(long long arr[], long long N,long long &count,int low,int high){
+    if(low<high){
+        int mid = low + (high-low)/2;
+        mergeSort(arr,N,count,low,mid);
+        mergeSort(arr,N,count,mid+1,high);
+        merge(arr,low,high,mid,count);
+    }
+}
+
+    long long int inversionCount(long long arr[], long long N)
     {
         // Your Code Here
-        long long int *temp=new long long int[N];
-        return merge(arr,0,N-1,temp);
+    long long count = 0;
+    int low = 0;
+    int high = N-1;
+    mergeSort(arr,N,count,low,high);
+    return count;
     }
 
 };
+
+
+// { Driver Code Starts.
+
+int main() {
+    
+    long long T;
+    cin >> T;
+    
+    while(T--){
+        long long N;
+        cin >> N;
+        
+        long long A[N];
+        for(long long i = 0;i<N;i++){
+            cin >> A[i];
+        }
+        Solution obj;
+        cout << obj.inversionCount(A,N) << endl;
+    }
+    
+    return 0;
+}
+  // } Driver Code Ends
